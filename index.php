611807314bb9043dc156a06aa69cb80b71d9a95a
Rithesh Rai <70254502+4SF18CS120@users.noreply.github.com>
2021-06-17 10:14:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>The Sparks Foundatiaon</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+      <div class="container-fluid">
+      <a class="navbar-brand text-success" href="#" padding="left" style="font-size: 30px;font-weight: 900;  ">MY BANKING </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="header">
+        <a class="w3-button w3-black w3-padding-large w3-large w3-margin-top" href="#services"></a></div>
+      <li class="nav-item"></li>
+      <ul class="navbar-nav ml-auto">
+              <a class="btn btn-danger navbar-btn mx-3 " href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-danger navbar-btn mx-3 " href="#">About Us</a>
+            </li>
+          <li class="nav-item">
+              <a class="btn btn-danger navbar-btn mx-3 " href="Transfer_money.php">Transfer Money</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-danger navbar-btn  mx-3" href="transactionhistory.php">Transaction History</a>
+            </li>
+</ul>
+        </div>
+      </div>
+    </nav>
+    <div class="main--body">
+        <div class="left--part">
+          <img src="img/bcg.png" alt="" srcset="">
+        </div>
+        <div class="main--section">
+          <div class="first">
+            <img src="img/customer.png"  height="70px" width="90px" alt="" srcset="">
+            <div class="button">
+           
+            <a href="Transfer_money.php"><button type="button" class="btn btn-outline-light" style="background-color :  #2785C4"> Customers </button></a>
+            </div>
+          </div>
+          <div class="second">
+            <div class="upper">
+              <img src="img/tranaction.png" alt="" srcset="">
+              <div class="button">
+              <a href="Transfer_money.php"><button type="button" class="btn btn-outline-light" style="background-color :  #2785C4";> Transfer Money </button></a>
+            </div>
+            </div>
+            <div class="lower">
+              <img src="img/tranactionhistory.png" alt="" srcset="">
+              <div class="button">
+              <a href="transactionhistory.php"><button type="button" class="btn btn-outline-light" style="background-color :  #2785C4";> Transaction History </button></a>
+            </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
